03feea8754e2d87a7d61b8fe463c5fa0a4b47483
DohyungKim <ehgud456456@naver.com>
2020-02-04 00:05:24 +0900

    dd
    dd

---
+++ view/navi.php
@@ -0,0 +1,14 @@
+<div class="wrap" id="nav_wrap">
+  <nav>
+    <ul>
+      <li><a href="./recruit.php">인재채용</a></li>
+      <li><a href="./business.php">비즈니스</a></li>
+      <li><a href="./creator.php">CREATOR</a></li>
+      <li><a href="./original.php">O&O</a></li>
+      <li style="float:left"><a href="./index.php">YOUNIV</a></li>
+    </ul>
+  </nav>
+</div>
+
+<div class="wrap" id="section_wrap">
+<section>
