84857eb837e321a1bebdf2b473c97565c79a64a8
DohyungKim <40906871+d5br5@users.noreply.github.com>
2020-08-24 00:05:40 +0900

    developer update

--- view/top.php
+++ view/top.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <META HTTP-EQUIV="refresh" CONTENT="600">
-    <link rel="shortcut icon" href="./img/140ci.png">
+    <link rel="shortcut icon" href="./img/youniv_favicon.png">
     <link href="./css/style.css" rel="stylesheet" type="text/css">
     <link href="./css/function.css" rel="stylesheet" type="text/css">
     <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
@@ -17,7 +17,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=otv3j9qrn4"></script>
     <script src="./js/navi.js"></script>
-    <!-- developer : dodo.kim @ youniv -->
+    <!-- developer : dohyung.kim -->
+    <!-- email : dohkim777@gmail.com -->
 </head>
 
 <body>
